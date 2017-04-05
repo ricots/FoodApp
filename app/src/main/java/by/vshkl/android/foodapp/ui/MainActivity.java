b42f88ad6e1ca07525d6bf4ebadd18437aaa2fa4
jgug <jgugn1@gmail.com>
2017-04-05 19:08:27 +0300

    Update main activity

---
+++ app/src/main/java/by/vshkl/android/foodapp/ui/MainActivity.java
@@ -0,0 +1,55 @@
+package by.vshkl.android.foodapp.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.arellomobile.mvp.presenter.InjectPresenter;
+
+import by.vshkl.android.foodapp.R;
+import by.vshkl.android.foodapp.mvp.presenter.MainPresenter;
+import by.vshkl.android.foodapp.mvp.view.MainView;
+
+public class MainActivity extends MvpAppCompatActivity implements MainView, OnClickListener {
+
+    @InjectPresenter MainPresenter presenter;
+
+    private FrameLayout flFragmentContainer;
+    private TextView tvEmpty;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        flFragmentContainer = (FrameLayout) findViewById(R.id.fl_fragment_container);
+        tvEmpty = (TextView) findViewById(R.id.tv_empty);
+
+        tvEmpty.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_empty:
+                presenter.downloadMenu();
+                break;
+        }
+    }
+
+    @Override
+    public void showEmpty() {
+        flFragmentContainer.setVisibility(View.GONE);
+        tvEmpty.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideEmpty() {
+        tvEmpty.setVisibility(View.GONE);
+        flFragmentContainer.setVisibility(View.VISIBLE);
+    }
+}
